49f04c383b0bf255148a649c5001df62d1c13f76
JYeonDev <whddus4837@gmail.com>
2022-01-08 23:52:21 +0900

    hedaer ing...

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,30 +6,34 @@ import styles from "./header.module.css";
 const Header = (props) => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.container}>
-          <div className={styles.title}>
-            <h1 className={styles.logo}>
-              <strong className={styles.strong}>C</strong>oin
-              <br />
-              <strong className={styles.strong}>U</strong>p
-            </h1>
-            <img
-              className={styles.logoImage}
-              src={"/images/logo.png"}
-              alt="logo"
-            />
-          </div>
-
-          <nav className={styles.nav}>
-            <Link to="/">실시간가격</Link>
-            <Link to="/">관심종목</Link>
-            <Link to="/">코인관련뉴스</Link>
-            <Link to="/">주요거래소</Link>
-            <Link to="/">About</Link>
-          </nav>
-        </div>
+      <div className={styles.title}>
+        <h1 className={styles.logo}>
+          <strong className={styles.strong}>C</strong>oin
+          <br />
+          <strong className={styles.strong}>U</strong>p
+        </h1>
+        <img className={styles.logoImage} src={"/images/logo.png"} alt="logo" />
       </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">실시간가격</Link>
+          </li>
+          <li>
+            <Link to="/">관심종목</Link>
+          </li>
+          <li>
+            <Link to="/">코인관련뉴스</Link>
+          </li>
+          <li>
+            <Link to="/">주요거래소</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
