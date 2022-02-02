b1755325c914075dc8bada82e5f22b42222ee87b
JYeonDev <whddus4837@gmail.com>
2022-02-02 23:43:05 +0900

    입출금내역 제작

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -4,6 +4,16 @@ import styles from "./wallet.module.css";
 const Wallet = () => {
   const [price, setPrice] = useState(0);
   const [hide, setHide] = useState(false);
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = ("0" + (date.getMonth() + 1)).slice(-2);
+  let day = ("0" + date.getDate()).slice(-2);
+  let dateString = year + "/" + month + "/" + day;
+
+  let hours = date.getHours();
+  let minutes = ("0" + date.getMinutes()).slice(-2);
+  let seconds = ("0" + date.getSeconds()).slice(-2);
+  let timeString = hours + ":" + minutes + ":" + seconds;
 
   function clickHide() {
     setHide(!hide);
@@ -29,10 +39,25 @@ const Wallet = () => {
           <p>보유자산 : {price} KRW</p>
           <div className={styles.input_container}>
             <input placeholder="금액을 입력해주세요" />
-            <button>확인</button>
+            <button>입금</button>
           </div>
         </div>
       )}
+      <div className={styles.exchange_list}>
+        <div className={styles.exchange}>입출금내역</div>
+        <div className={styles.exchange_text}>
+          <p className={styles.title}>날짜</p>
+          <p className={styles.title}>내용</p>
+          <p className={styles.title}>금액</p>
+        </div>
+        <div className={styles.exchange_text}>
+          <p className={styles.time}>
+            {dateString} {timeString}
+          </p>
+          <p className={styles.memo}></p>
+          <p className={styles.money}>{price}</p>
+        </div>
+      </div>
     </div>
   );
 };
