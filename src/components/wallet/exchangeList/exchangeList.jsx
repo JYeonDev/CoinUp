20e0f396df5b818e55e43b8179c27f01d9238d25
JYeonDev <whddus4837@gmail.com>
2022-02-09 23:27:54 +0900

    list 재구성

--- src/components/wallet/exchangeList/exchangeList.jsx
+++ src/components/wallet/exchangeList/exchangeList.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import styles from "./exchangeList.module.css";
 
-const ExchangeList = ({ date, memo, price, result }) => {
-  return (
-    <div className={styles.exchange_container}>
-      <div className={styles.exchange_list}>
-        <div className={styles.item_block}>
-          <div>{date}날짜</div>
-          <div>{memo}메모</div>
-          <div>{price}가격</div>
-          <div>{result}입출금</div>
-        </div>
-      </div>
-    </div>
-  );
+const ExchangeList = ({ item }) => {
+  console.log(item);
+  return <ul>{item}</ul>;
 };
 
 export default ExchangeList;
--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -4,33 +4,33 @@ import ExchangeList from "./exchangeList/exchangeList";
 import styles from "./wallet.module.css";
 
 const Wallet = () => {
-  const [price, setPrice] = useState(0);
-  const [money, setMoney] = useState(0);
   const [hide, setHide] = useState(false);
-  const [exchange, setExchange] = useState([]);
-  const inputRef = useRef();
-
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = ("0" + (date.getMonth() + 1)).slice(-2);
-  let day = ("0" + date.getDate()).slice(-2);
-  let dateString = year + "/" + month + "/" + day;
-
-  let hours = date.getHours();
-  let minutes = ("0" + date.getMinutes()).slice(-2);
-  let seconds = ("0" + date.getSeconds()).slice(-2);
-  let timeString = hours + ":" + minutes + ":" + seconds;
 
   function clickHide() {
     setHide(!hide);
   }
 
-  function onClick() {
-    let exchangeCopy = [...exchange];
-    exchangeCopy.unshift(money);
-    setExchange(exchangeCopy);
-    setMoney(inputRef.current.value);
-    setPrice(Number(inputRef.current.value));
+  const [item, setItem] = useState([]);
+
+  const date = new Date();
+  const time = date.getTime();
+
+  const inputRef = useRef();
+  console.log(inputRef);
+  function addItem() {
+    if (inputRef.current.value) {
+      const itemCopy = [...item];
+      itemCopy.push(
+        <li>
+          <span>{time}</span>
+          <span>시간</span>
+          <span>금액</span>
+          {inputRef.current.value}
+        </li>
+      );
+      setItem([itemCopy]);
+      inputRef.current.value = "";
+    }
   }
 
   return (
@@ -38,7 +38,7 @@ const Wallet = () => {
       <div className={styles.property}>
         <span className={styles.total}>총 보유자산</span>
         <span>
-          <span className={styles.price}>{money}</span> KRW
+          <span className={styles.price}>0</span> KRW
         </span>
       </div>
       <div className={styles.divide}>
@@ -50,42 +50,27 @@ const Wallet = () => {
       {hide ? null : (
         <div className={styles.deposit_page}>
           <p>하루 이체한도 : 200,000,000 KRW</p>
-          <p>보유자산 : {price} KRW</p>
-          <div className={styles.input_container}>
-            <form>
-              <input
-                type="text"
-                placeholder="금액을 입력해주세요"
-                autoFocus
-                ref={inputRef}
-              />
-              <button onClick={onClick}>입금</button>
-            </form>
-          </div>
+          <p>보유자산 : 0 KRW</p>
+          <div className={styles.input_container}></div>
         </div>
       )}
-      <div className={styles.exchange_list}>
-        <div className={styles.exchange}>입출금내역</div>
-        <div className={styles.exchange_text}>
-          <p className={styles.title}>날짜</p>
-          <p className={styles.title}>내용</p>
-          <p className={styles.title}>금액</p>
-          <p className={styles.title}>입/출금</p>
-        </div>
-        {/* {exchange.map((list) => {
-          return (
-            <div className={styles.exchange_text}>
-              <p className={styles.time}>
-                {dateString} {timeString}
-              </p>
-              <p className={styles.memo}></p>
-              <p className={styles.money}>{list} 원</p>
-              <p>입/출금</p>
-            </div>
-          );
-        })} */}
-        <ExchangeList />
-      </div>
+
+      {/* todolist 쓸곳 */}
+
+      <input
+        className={styles.inputText}
+        type="text"
+        placeholder="금액을 입력하세요."
+        ref={inputRef}
+      />
+      <input
+        type="button"
+        value="입력"
+        onClick={() => {
+          addItem();
+        }}
+      />
+      <ExchangeList item={item} />
     </div>
   );
 };
